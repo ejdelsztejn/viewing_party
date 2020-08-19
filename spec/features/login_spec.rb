cc4d4058dda19a6300e96aa78a88266f15aae86b
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-19 18:47:09 -0400

    Update login spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Login' do
   describe 'As a user' do
     describe 'When I visit the root path' do
       before(:each) do
+        OmniAuth.config.mock_auth[:google] = nil
         OmniAuth.config.test_mode = true
         OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
           :uid => 'mock_uid',
@@ -19,6 +20,15 @@ RSpec.describe 'Login' do
         visit '/'
 
         expect(page).to have_button('Log In with Google')
+      end
+      it 'I should have a new user session' do
+        visit '/'
+
+        expect(page).to have_button('Log In with Google')
+        # click_on 'Log In with Google'
+
+        require "pry"; binding.pry
+        OmniAuth.config.mock_auth[:google]
       end
     end
   end
