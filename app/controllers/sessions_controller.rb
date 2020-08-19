152e6a1a457d772f3baf023efb57c5f313ec2e92
Sienna Kopf <princess.kopf@gmail.com>
2020-08-19 15:39:36 -0600

    Add sessions controller with omniauth action that handles user session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.valid?
       session[:user_id] = user.id
-      redirect_to "/"
+      redirect_to "/dashboard"
     else
       redirect_to '/login'
     end
