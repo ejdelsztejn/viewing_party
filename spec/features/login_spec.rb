c8cba4ce884794f039e26632df0d460f80b65afb
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-19 18:26:09 -0400

    Add omniauth test mode

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe 'Login' do
   describe 'As a user' do
     describe 'When I visit the root path' do
       before(:each) do
-        require "pry"; binding.pry
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+          :uid => 'mock_uid',
+          :info => {
+            :email => 'mock_user_email'
+          },
+          :credentials => {
+            :token => 'mock_token'
+          }
+        })
       end
       it 'I should see a button to Log In with Google' do
         visit '/'
 
-        expect(page).to have_content('Welcome to Viewing Party')
+        expect(page).to have_button('Log In with Google')
       end
     end
   end
