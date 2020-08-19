9c0d8c414a2d4a80c3f2b5fbf2d150f100b96ff7
Sienna Kopf <princess.kopf@gmail.com>
2020-08-19 15:42:43 -0600

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_134959) do
+ActiveRecord::Schema.define(version: 2020_08_19_213034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "uid"
+    t.string "token"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "uid"
-    t.string "provider"
   end
 
 end
