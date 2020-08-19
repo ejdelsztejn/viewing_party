5f581e581692cbf38e2ab9af797bf1a73fd1c9db
Sienna Kopf <princess.kopf@gmail.com>
2020-08-19 15:41:20 -0600

    Add user model with from_omniauth class method that handles user authentication through omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  # has_secure_password
-
+  
   def self.from_omniauth(response)
-    User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
-      u.name = response[:info][:name]
-      u.email = response[:info][:email]
-      # u.password = SecureRandom.hex(15)
-    end
+    user = User.find_or_create_by(uid: response[:uid])
+    user.email = response[:info][:email]
+    user.uid = response[:uid]
+    user.token = response[:credentials][:token]
+    user.save
   end
 end
