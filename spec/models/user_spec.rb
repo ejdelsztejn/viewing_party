af673b7f6a1c5eec9fd83dc4254d106f068f72ef
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-20 13:59:42 -0400

    Add relationships test to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe User do
     it {should validate_presence_of :token}
   end
 
+  describe 'relationships' do
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
+  end
+
   describe 'methods' do
     before :each do
       @response = {
