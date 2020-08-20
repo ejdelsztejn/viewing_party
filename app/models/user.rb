1af9a919cf4dd645d49f6d4c5b6c4be9c9303a28
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-20 13:13:43 -0400

    Add friendship model and relationships to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   def self.from_omniauth(response)
     user = User.find_or_create_by(uid: response[:uid])
@@ -10,4 +11,9 @@ class User < ApplicationRecord
     user.save
     user
   end
+end
+
+class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
 end
