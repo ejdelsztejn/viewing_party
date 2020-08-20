ab0780d4426d99304280861037b0c234a8fd3141
Sienna Kopf <princess.kopf@gmail.com>
2020-08-20 12:07:59 -0600

    Move oAuth mock auth into rails helper file to have access for all tests

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'user dashboard page' do
-  before(:each) do
-    OmniAuth.config.mock_auth[:google_oauth2] = nil
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-      :uid => 'mock_uid',
-      :info => {
-        :name => 'Mock User',
-        :email => 'mock_user_email'
-      },
-      :credentials => {
-        :token => 'mock_token'
-      }
-    })
-  end
   it 'displays a button to discover movies, a friends section, and a viewing parties section' do
     visit '/'
 
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,20 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Login' do
   describe 'As a user' do
     describe 'When I visit the root path' do
-      before(:each) do
-        OmniAuth.config.mock_auth[:google_oauth2] = nil
-        OmniAuth.config.test_mode = true
-        OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-          :uid => 'mock_uid',
-          :info => {
-            :name => 'Mock User',
-            :email => 'mock_user_email'
-          },
-          :credentials => {
-            :token => 'mock_token'
-          }
-        })
-      end
       it 'I should see a button to Log In with Google' do
         visit '/'
 
