461c2aedcdd7bc7e46ad2670ac0afbfc45baffbf
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-19 22:27:25 -0400

    Complete ouath test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,7 @@ class SessionsController < ApplicationController
   def omniauth
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user
-      # session[:user_id] = user.id
-      # redirect_to "/dashboard"
+      session[:user_id] = user.id
       redirect_to '/dashboard'
     else
       redirect_to '/login'
