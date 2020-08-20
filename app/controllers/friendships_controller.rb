8e5d55a8c43bd85121f8a0f7ad431e1ea5d3017e
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-20 16:20:07 -0400

    Fix rubocop offenses

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,10 +3,9 @@ class FriendshipsController < ApplicationController
     friend = User.find_by(email: params[:email])
     if friend
       Friendship.create_reciprocal_for_ids(current_user.id, friend.id)
-      redirect_to "/dashboard"
     else
-      flash[:errors] = "Friend does not exist in system. Cannot add."
-      redirect_to "/dashboard"
+      flash[:errors] = 'Friend does not exist in system. Cannot add.'
     end
+    redirect_to '/dashboard'
   end
 end
