3f5f5fa48c4ca1e3f48f9d6f94135a4c7a4629be
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-20 12:55:03 -0400

    Add name to create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(response)
     user = User.find_or_create_by(uid: response[:uid])
+    user.name = response[:info][:name]
     user.email = response[:info][:email]
     user.uid = response[:uid]
     user.token = response[:credentials][:token]
