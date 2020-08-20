2dff2b0673ba2c09a5769864eab4555f0955b62c
Sienna Kopf <princess.kopf@gmail.com>
2020-08-20 14:10:07 -0600

    Add conditional logic within create method to handle sad path

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,12 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:email])
-    Friendship.create_reciprocal_for_ids(current_user.id, friend.id)
-    redirect_to "/dashboard"
+    if friend
+      Friendship.create_reciprocal_for_ids(current_user.id, friend.id)
+      redirect_to "/dashboard"
+    else
+      flash[:errors] = "Friend does not exist in system. Cannot add."
+      redirect_to "/dashboard"
+    end
   end
 end
