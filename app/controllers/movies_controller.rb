87dd7a5317caebc4a0f46ed20b001af8e8f848f3
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-21 14:16:29 -0400

    Update before push:

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,15 @@
 class MoviesController < ApplicationController
-  def index; end
+  def index
+    search = "fight"
+
+    conn = Faraday.new(url: "https://api.themoviedb.org")
+
+    response = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&query=#{search}&page=1&include_adult=false")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    require "pry"; binding.pry
+    # @members = json[:results]
+
+    # Make movie object (PORO) that can be sent to the view
+  end
 end
