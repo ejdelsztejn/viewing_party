2f1d34232c30603f773f78d6a9d6380531835ffd
Sienna Kopf <princess.kopf@gmail.com>
2020-08-21 15:41:43 -0600

    Add conditional logic and partial refactor to the movies controller.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,28 @@
 class MoviesController < ApplicationController
   def index
-    search = 'fight'
-    
-    conn = Faraday.new(url: "https://api.themoviedb.org")
+    if params.keys.include?("keyword")
+      keyword = params[:keyword]
+      search_facade = SearchFacade.new(keyword)
+      @movies = search_facade.search_movies
 
-    response = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&query=#{search}&page=1&include_adult=false")
+      # search_word = params[:keyword]
+      # conn = Faraday.new(url: "https://api.themoviedb.org")
+      #
+      # response = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&query=#{search_word}&page=1&include_adult=false")
+      #
+      # json = JSON.parse(response.body, symbolize_names: true)
+    else
+      search_word = "happy"
+      conn = Faraday.new(url: "https://api.themoviedb.org")
 
-    json = JSON.parse(response.body, symbolize_names: true)
+      response = conn.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&query=#{search_word}&page=1&include_adult=false")
 
-    @movies = json[:results].map do |movie_info|
-      Movie.new(movie_info)
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      @movies = json[:results].map do |movie_info|
+        Movie.new(movie_info)
+      end
     end
-
     # Make movie object (PORO) that can be sent to the view
-  end
-
-  def search
-    params[:keyword]
   end
 end
