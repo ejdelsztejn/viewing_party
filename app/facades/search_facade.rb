7fc6f7314db277a23dcad369c2fafcea41b60571
Sienna Kopf <princess.kopf@gmail.com>
2020-08-21 15:43:54 -0600

    Add facade to handle movie object createion and display

---
+++ app/facades/search_facade.rb
@@ -0,0 +1,20 @@
+class SearchFacade
+  attr_reader :keyword
+
+  def initialize(keyword)
+    @keyword = keyword
+    @movies_service = MoviesService.new
+  end
+
+  def search_movies
+    movies_info = get_search_movies
+
+    @movies = movies_info[:results].map do |movie_info|
+      Movie.new(movie_info)
+    end
+  end
+
+  def get_search_movies
+    @movies_service.search_movies(@keyword)
+  end
+end
