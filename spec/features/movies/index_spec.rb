8558a0abf78fe5be7cb2e4cb8f19dbee01406b88
Sienna Kopf <princess.kopf@gmail.com>
2020-08-22 16:10:51 -0600

    Add test for viewing generic movies

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'find movies' do
+  describe 'As an authenticated user' do
+    it 'I can link from the dashboard to the movies page' do
+      visit '/'
+
+      click_on "Movies"
+
+      expect(current_path).to eq("/movies")
+
+      expect(page).to have_css(".movie", count: 40)
+
+      within(first(".movie")) do
+        expect(page).to have_css(".title")
+        name = find('.title').text
+        expect(name).not_to be_empty
+
+        ## Not sure how to call this as a link because the movie obj aren't being saved to the DB
+        # expect(page).to have_link(find('.title').text)
+
+        expect(page).to have_css(".vote_average")
+        name = find('.vote_average').text
+        expect(name).not_to be_empty
+      end
+    end
+  end
+end
