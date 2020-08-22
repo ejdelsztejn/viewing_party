6bb60a3e6aecca18a4c36d881cf8a7186f0e04d9
Sienna Kopf <princess.kopf@gmail.com>
2020-08-22 15:15:46 -0600

    Add test for linking to a discover movies page from the dashboard

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -2,24 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'discover movies page' do
   describe 'As an authenticated user' do
-    it 'I should see a button to discover top-rated movies' do
+    it 'I can link to the discover page from the dashboard' do
       visit '/'
 
       click_on 'Log In with Google'
 
       expect(current_path).to eq('/dashboard')
 
-      within 'nav' do
-        expect(page).to have_button('Discover Top-rated Movies')
-        expect(page).to have_content('Keyword')
-        expect(page).to have_button('Find Movies')
-      end
-
+      expect(page).to have_link("Discover Movies")
       click_on "Discover Movies"
 
-      expect(current_path).to eq('/movies')
-
-      save_and_open_page
+      expect(current_path).to eq("/movies/discover")
     end
   end
 end
