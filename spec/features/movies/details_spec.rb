48682757c8adcbd6cacd6cc25e17d9632089e552
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-22 19:26:16 -0400

    Add movie details page spec page

---
+++ spec/features/movies/details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'movie details page' do
+  describe 'As an authenticated user' do
+    before :each do
+      visit '/'
+
+      click_on 'Log In with Google'
+    end
+    describe 'When I visit the movies page' do
+      it "And I click on the movie title link, I should be redirected to the movie's details page" do
+        visit '/movies/discover'
+
+        expect(page).to have_button("Discover Top-rated Movies")
+        click_on 'Discover Top-rated Movies'
+
+        expect(page).to have_link('The Shawshank Redemption')
+        click_on 'The Shawshank Redemption'
+
+        expect(current_path).to eq("/movies/#{shawshank_redemption.id}/details")
+        expect(page).to have_content('The Shawshank Redemption')
+      end
+    end
+  end
+end
