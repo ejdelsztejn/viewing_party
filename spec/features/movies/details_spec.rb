7025dac9ccbc56b38bad6631fb2e237366f2aca4
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-22 20:54:18 -0400

    Add outline for second test in movie details page spec

--- spec/features/movies/details_spec.rb
+++ spec/features/movies/details_spec.rb
@@ -22,5 +22,28 @@ RSpec.describe 'movie details page' do
         expect(page).to have_content('The Shawshank Redemption')
       end
     end
+    describe "When I visit the movie's detail page" do
+      it 'I should see a button to create a viewing party,
+          and I should see the following information' do
+
+        visit '/movies/discover'
+
+        expect(page).to have_button("Discover Top-rated Movies")
+        click_on 'Discover Top-rated Movies'
+
+        expect(page).to have_link('The Shawshank Redemption')
+        click_on 'The Shawshank Redemption'
+
+        expect(page).to have_content('The Shawshank Redemption')
+        expect(page).to have_content('Vote average: 8.7')
+        expect(page).to have_content('Runtime: 142 minutes')
+        expect(page).to have_content('Genres: Drama, Crime')
+        expect(page).to have_content('Summary: Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope')
+        # first 10 cast members
+        # count of total reviews
+        # each review author and information
+        
+      end
+    end
   end
 end
