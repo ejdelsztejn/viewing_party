9ed6bb0f31f8b322c7fc03bf0510951c6ff9d9e7
Sienna Kopf <princess.kopf@gmail.com>
2020-08-23 11:29:14 -0600

    Add services test for new movie details page instance methods

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -35,5 +35,40 @@ describe MoviesService do
         expect(movie_data).to have_key :vote_average
       end
     end
+    context "#general_movie_details" do
+      it "returns movie data based on a keyword search" do
+        service = MoviesService.new
+        search_movie_details = service.general_movie_details("278")
+        expect(search_movie_details).to be_a Hash
+        expect(search_movie_details).to have_key :id
+        expect(search_movie_details).to have_key :title
+        expect(search_movie_details).to have_key :vote_average
+        expect(search_movie_details).to have_key :runtime
+        expect(search_movie_details).to have_key :genres
+        expect(search_movie_details[:genres]).to be_a Array
+        expect(search_movie_details[:genres].first).to be_a Hash
+      end
+    end
+    context "#movie_reviews" do
+      it "returns movie data based on a keyword search" do
+        service = MoviesService.new
+        search_reviews = service.movie_reviews("278")
+        expect(search_reviews).to be_a Hash
+        expect(search_reviews[:results]).to be_an Array
+        review_data = search_reviews[:results].first
+        expect(review_data).to have_key :author
+        expect(review_data).to have_key :content
+      end
+    end
+    context "#movie_cast" do
+      it "returns movie data based on a keyword search" do
+        service = MoviesService.new
+        search_cast = service.movie_cast("278")
+        expect(search_cast).to be_a Hash
+        expect(search_cast[:cast]).to be_an Array
+        cast_data = search_cast[:cast].first
+        expect(cast_data).to have_key :name
+      end
+    end
   end
 end
