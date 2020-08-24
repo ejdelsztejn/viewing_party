1d7a9af4b8f32648b0d27384fb2ed9346f1b142b
Sienna Kopf <princess.kopf@gmail.com>
2020-08-24 14:57:54 -0600

    Configure VCR and refigure to use it in the discover movies spec

--- spec/features/dashboard/discover_movies_spec.rb
+++ spec/features/dashboard/discover_movies_spec.rb
@@ -3,20 +3,21 @@ require 'rails_helper'
 RSpec.describe 'from the dashboard page' do
   describe 'go to movies' do
     it 'displays a button to view movies' do
-      json_response = File.read('spec/fixtures/generic_movies.json')
-      stub_request(:get, "https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&page=1").
-        to_return(status: 200, body: json_response)
+      # json_response = File.read('spec/fixtures/generic_movies.json')
+      # stub_request(:get, "https://api.themoviedb.org/3/movie/popular?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&page=1").
+      #   to_return(status: 200, body: json_response)
+      VCR.use_cassette('generic_movies') do
+        visit '/'
 
-      visit '/'
+        click_on 'Log In with Google'
 
-      click_on 'Log In with Google'
+        expect(current_path).to eq('/dashboard')
 
-      expect(current_path).to eq('/dashboard')
+        click_on "Movies"
 
-      click_on "Movies"
-
-      expect(current_path).to eq('/movies')
-      expect(page).to have_content('Discover Movies')
+        expect(current_path).to eq('/movies')
+        expect(page).to have_content('Discover Movies')
+      end
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -81,3 +81,10 @@ OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
     :token => 'mock_token'
   }
 })
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<MOVIES_API_KEY>') { ENV['MOVIES_API_KEY'] }
+  config.configure_rspec_metadata!
+end
