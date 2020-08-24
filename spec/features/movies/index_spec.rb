cd1c93a0eba1ef9542ff86c3ee7a7c18697b0192
Sienna Kopf <princess.kopf@gmail.com>
2020-08-24 15:08:13 -0600

    Mock out movies index for generic movies

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -9,35 +9,39 @@ RSpec.describe 'find movies' do
     end
 
     it 'displays a button for finding top_rated movies and a search bar' do
-      visit "/movies"
+      VCR.use_cassette('generic_movies') do
+        visit "/movies"
 
-      expect(page).to have_button("Discover Top-rated Movies")
+        expect(page).to have_button("Discover Top-rated Movies")
 
-      within ".search_form" do
-        expect(page).to have_field(:keyword)
+        within ".search_form" do
+          expect(page).to have_field(:keyword)
+        end
       end
     end
 
     it 'I can link from the dashboard to the movies page' do
-      expect(current_path).to eq("/dashboard")
+      VCR.use_cassette('generic_movies') do
+        expect(current_path).to eq("/dashboard")
 
-      click_on "Movies"
+        click_on "Movies"
 
-      expect(current_path).to eq("/movies")
+        expect(current_path).to eq("/movies")
 
-      expect(page).to have_css(".movie", count: 20)
+        expect(page).to have_css(".movie", count: 20)
 
-      within(first(".movie")) do
-        expect(page).to have_css(".title")
-        name = find('.title').text
-        expect(name).not_to be_empty
+        within(first(".movie")) do
+          expect(page).to have_css(".title")
+          name = find('.title').text
+          expect(name).not_to be_empty
 
-        expect(page).to have_link(find('.title-movie').text)
+          expect(page).to have_link(find('.title-movie').text)
 
-        expect(page).to have_css(".vote_average")
-        name = find('.vote_average').text
-        expect(name).not_to be_empty
-      end
+          expect(page).to have_css(".vote_average")
+          name = find('.vote_average').text
+          expect(name).not_to be_empty
+        end
+      end 
     end
   end
 end
