de1d09eb219248a57f0b0c131ccc9877230b511c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-25 17:29:19 -0400

    Change time column to end_time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_212726) do
+ActiveRecord::Schema.define(version: 2020_08_25_212829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_212726) do
     t.string "movie_title"
     t.integer "duration_of_party"
     t.datetime "start_time"
-    t.datetime "time"
+    t.datetime "end_time"
     t.integer "friend_ids", array: true
     t.bigint "user_id"
     t.datetime "created_at", null: false
